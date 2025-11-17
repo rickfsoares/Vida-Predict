ddd183344223301bb09d6bae20775e3e0b1cbe2d
ricardo frança soares <rickfsoaresdev@gmail.com>
2025-11-17 14:58:52 -0300

    feat: add logic to check datasets

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-from pysus import SIA
 import pandas as pd
-
-sia = SIA().load()
-
-files = sia.get_files("PA", uf="SP", year=2022)
-
-sia.download(files, local_dir="./dados-datasus-processados/")
-
-#parquet = sia.download(files, local_dir="./dados-datasus-processados/")[0]
-
-#print(parquet.to_dataframe())
-
 
 #df = pd.read_parquet("./dados-datasus-processados/PASP0001.parquet/")
 
-#df = pd.read_parquet("./dados-datasus-processados/PASP1801a.parquet/")
+df = pd.read_parquet("./dados-datasus-processados/AMSP2201.parquet")
+print("Info APAC de Medicamentos")
+print(df.info())
+print("Describe")
+print(df.describe())
+
+df2 = pd.read_parquet("./dados-datasus-processados/ADSP2201.parquet")
+print("Info APAC de Laudos Diversos")
+print(df2.info())
+print("describe")
+print(df2.describe())
+print()
+
+df3 = pd.read_parquet("./dados-datasus-processados/BISP2201.parquet")
+print("Info Boletim de Produção Ambulatorial individualizado")
+print(df3.info())
+print("describe")
+print(df3.describe())
+
+#df = pd.read_parquet("./dados-unificados/medicamentos_features.parquet")
+#print("Info")
+#print(df.info())
+#print("DESCRIBE")
+#print(df.describe())
+#print()
 #print(df.head())
