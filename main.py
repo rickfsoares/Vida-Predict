62c5f9127686a7e8cb1f171bdeeef4ad85814e61
ricardo frança soares <rickfsoaresdev@gmail.com>
2025-11-18 23:06:41 -0300

    feat: add tests in dataset

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ print(laudos_df.describe())
 print(laudos_df.head())
 
 print()
+print("-=" * 10)
 
 historico = pd.read_parquet("./dados-unificados/historico_familiar.parquet")
 print("Historico")
@@ -15,9 +16,22 @@ print(historico.describe())
 print(historico.head())
 
 print()
+print("-=" * 10)
 
 medicamentos_df = pd.read_parquet("./dados-unificados/medicamentos_features.parquet")
 print("Medicamentos")
 print(medicamentos_df.info())
 print(medicamentos_df.describe())
 print(medicamentos_df.head())
+
+print()
+print("-=" * 10)
+
+df_final = pd.read_parquet("./dados-unificados/dataset_final_modelo.parquet")
+print("Final")
+print(df_final.info())
+print(df_final.describe())
+print(df_final.head().to_string())
+print(df_final["TEM_DM1"].unique())
+
+
